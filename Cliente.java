366b776fba3c7e732f568bedaf9e5c66602f9fe4
Alex <alex.fraga@estudante.ifto.edu.br>
2024-05-10 16:17:12 -0300

    Projeto Banco

---
+++ Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente {
+    private String nome;
+    public void setNome(String nome) {
+		this.nome = nome;
+	}
+    public String Nome() {
+		return nome;
+	}
+}
---
+++ ContaPoupanca.java
@@ -0,0 +1,16 @@
+
+public class ContaPoupanca extends Conta {
+
+	private static final String contas = null;
+
+
+    public ContaPoupanca(Cliente cliente) {
+		super(cliente);
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Poupança ===");
+		super.imprimirInfosComuns();
+	}
+}
